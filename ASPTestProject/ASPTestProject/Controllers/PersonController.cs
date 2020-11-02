9bffd798830c44a0990b75d4b48f1063461f3431
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-11-02 11:51:56 -0700

    Added fully functional Create and Delete person functions;

--- ASPTestProject/ASPTestProject/Controllers/PersonController.cs
+++ ASPTestProject/ASPTestProject/Controllers/PersonController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using ASPTestProject.Models;
@@ -11,17 +12,43 @@ namespace ASPTestProject.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            Debug.WriteLine("Action - Index Action");
+            return RedirectToAction("Management");
         }
 
         public IActionResult Management()
         {
+            /*  People.Add(new Person() { FirstName = "Test", LastName = "Tester" });
+                People.Add(new Person() { FirstName = "Person", LastName = "Manager" });
+            */
+            Debug.WriteLine("Action - Management Action");
+
+            ViewBag.People = People;
             return View();
         }
 
+        public IActionResult Create(string firstName, string lastName)
+        {
+            Debug.WriteLine($"DATA - Create Person");
+
+            CreatePerson(firstName, lastName);
+            return RedirectToAction("Management");
+        }
+
+        public IActionResult Delete(string firstName, string lastName)
+        {
+            Debug.WriteLine($"DATA - Delete Person {firstName}");
+
+            DeletePersonByFirstName(firstName);
+            return RedirectToAction("Management");
+        }
+
         public static List<Person> People = new List<Person>();
+
+        // These methods are for data management. The body of the methods will be replaced with EF code tomorrow, but  for now we're just using a static list. 
         public void CreatePerson(string firstName, string lastName)
         {
+            Debug.WriteLine($"DATA - CreatePerson({firstName}, {lastName})");
             People.Add(new Person()
             {
                 FirstName = firstName.Trim(),
