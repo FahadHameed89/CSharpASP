2bcdd671a08fd7cd45cc287bd96e25747dddd687
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-11-02 09:26:08 -0700

    Setup MVC Project;

---
+++ ASPTestProject/ASPTestProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPTestProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
