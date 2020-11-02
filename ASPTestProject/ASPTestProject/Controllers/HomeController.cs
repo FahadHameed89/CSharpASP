136c5f5468488e76ca86d026c8dbc265acfb305e
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-11-02 09:57:05 -0700

    Created a Test page;

--- ASPTestProject/ASPTestProject/Controllers/HomeController.cs
+++ ASPTestProject/ASPTestProject/Controllers/HomeController.cs
@@ -11,6 +11,18 @@ namespace ASPTestProject.Controllers
 {
     public class HomeController : Controller
     {
+        // Controllers are responsible fo rfetching and returning views according to URIs as well as performing data manipulation via an EF Context
+
+        // A method that will return a View (whether directly or through a redirect) is referred to as an 'Action', and will typically return an IActionResult
+
+        // A data manipulation method will look more similar to our EF practice done with a console application.
+
+        // With the exception of Home/Index which renders at the root path), pages render at /ControllerName/ActionName ("Home/Privacy for this page's privacy action"). 
+        
+        // This is also configurable if you want to change it. 
+
+        // 1 - If we make a new TestPage() and travel to it (Run project -> Add /home/TestPage to end of file extension) you will see a blank page, then an ERROR because we haven't made the page yet. 
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -20,10 +32,16 @@ namespace ASPTestProject.Controllers
 
         public IActionResult Index()
         {
+            // 'return View()' essentially means just get the view associated with this path and send it to the client. 
             return View();
         }
 
         public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult TestPage()
         {
             return View();
         }
